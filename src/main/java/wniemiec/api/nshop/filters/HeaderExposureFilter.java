8b896ebdd8fc4563be88688567c8496dd33b32b5
William Niemiec <williamniemiec@hotmail.com>
2021-12-31 09:53:24 -0300

    Refactoring

--- src/main/java/wniemiec/api/nshop/filters/HeaderExposureFilter.java
+++ src/main/java/wniemiec/api/nshop/filters/HeaderExposureFilter.java
@@ -1,27 +1,28 @@
 package wniemiec.api.nshop.filters;
 
 import org.springframework.stereotype.Component;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+/**
+ * Responsible for intercepting and configuring headers from http requests.
+ */
 @Component
 public class HeaderExposureFilter implements Filter {
 
+    //-------------------------------------------------------------------------
+    //		Methods
+    //-------------------------------------------------------------------------
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, 
+                         ServletResponse servletResponse, 
+                         FilterChain filterChain)
+    throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) servletResponse;
+        
         res.addHeader("access-control-expose-headers", "location");
         filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
     }
 }
